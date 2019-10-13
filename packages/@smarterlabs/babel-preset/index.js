0ca1f789cfcbd406c51d1ff4d64c99e6519354b9
Kennedy Rose <kennedyianrose@gmail.com>
2019-10-13 16:59:42 -0500

    Babel preset function

--- packages/@smarterlabs/babel-preset/index.js
+++ packages/@smarterlabs/babel-preset/index.js
@@ -1,67 +1,77 @@
-module.exports = {
-	parser: `babel-eslint`,
-	extends: [
-		`eslint:recommended`,
-		`plugin:react/recommended`,
-		`plugin:jest/recommended`,
-	],
-	env: {
-		es6: true,
-		node: true,
-	},
-	settings: {
-		'import/core-modules': [
-			`styled-jsx`,
-			`styled-jsx/css`,
-		],
-	},
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 2018,
-		sourceType: `module`,
-	},
-	plugins: [
-		`react`,
-		`jest`,
-	],
-	globals: {
-		graphql: true,
-		window: true,
-		document: true,
-		localStorage: true,
-	},
-	rules: {
-		indent: [
-			`error`,
-			`tab`,
-			{
-				SwitchCase: 1,
-			},
-		],
-		'linebreak-style': [
-			`error`,
-			`unix`,
-		],
-		quotes: [
-			`error`,
-			`backtick`,
-		],
-		semi: [
-			`error`,
-			`never`,
-		],
-		'comma-dangle': [
-			`error`,
-			`always-multiline`,
-		],
-		'no-console': 0,
-		'require-atomic-updates': 0,
-		'no-unused-expressions': 0,
-		'react/jsx-uses-vars': 1,
-		'react/prop-types': 0,
-		'react/no-unescaped-entities': 0,
-		'react/display-name': 0,
-	},
+'use strict'
+const { declare } = require(`@babel/helper-plugin-utils`)
+
+const defaultTargets = {
+	android: 30,
+	chrome: 35,
+	edge: 14,
+	explorer: 9,
+	firefox: 52,
+	safari: 8,
+	ucandroid: 1,
 }
+
+function buildTargets({ additionalTargets }) {
+	return Object.assign({}, defaultTargets, additionalTargets)
+}
+
+module.exports = declare((api, options) => {
+	// see docs about api at https://babeljs.io/docs/en/config-files#apicache
+	api.assertVersion(7)
+
+	const {
+		modules,
+		targets = buildTargets(options),
+	} = options
+
+	if (typeof modules !== `undefined` && typeof modules !== `boolean` && modules !== `auto`) {
+		throw new TypeError(`babel-preset-escalade only accepts \`true\`, \`false\`, or \`"auto"\` as the value of the "modules" option`)
+	}
+
+	const debug = typeof options.debug === `boolean` ? options.debug : false
+	const development = typeof options.development === `boolean`
+		? options.development
+		: api.cache.using(() => process.env.NODE_ENV === `development`)
+
+	return {
+		presets: [
+			[require(`@babel/preset-env`), {
+				debug,
+				targets,
+				modules: modules === false ? false : `auto`,
+			}],
+			[require(`@babel/preset-react`), { development }],
+		],
+		plugins: [
+			require(`@babel/plugin-transform-runtime`),
+			require(`@babel/plugin-proposal-function-bind`),
+			require(`@babel/plugin-proposal-export-default-from`),
+			require(`@babel/plugin-proposal-logical-assignment-operators`),
+			[require(`@babel/plugin-proposal-optional-chaining`), {
+				loose: false,
+			}],
+			[require(`@babel/plugin-proposal-pipeline-operator`), {
+				proposal: `minimal`,
+			}],
+			[require(`@babel/plugin-proposal-nullish-coalescing-operator`), {
+				loose: false,
+			}],
+			require(`@babel/plugin-proposal-do-expressions`),
+			[require(`@babel/plugin-proposal-decorators`), {
+				legacy: true,
+			}],
+			require(`@babel/plugin-proposal-function-sent`),
+			require(`@babel/plugin-proposal-export-namespace-from`),
+			require(`@babel/plugin-proposal-numeric-separator`),
+			require(`@babel/plugin-proposal-throw-expressions`),
+			require(`@babel/plugin-syntax-dynamic-import`),
+			require(`@babel/plugin-syntax-import-meta`),
+			[require(`@babel/plugin-proposal-class-properties`), {
+				loose: false,
+			}],
+			require(`@babel/plugin-proposal-json-strings`),
+			require(`babel-plugin-dynamic-import-node`),
+			require(`@babel/plugin-proposal-object-rest-spread`),
+		],
+	}
+})
