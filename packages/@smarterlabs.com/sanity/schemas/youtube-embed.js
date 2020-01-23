4810fc364054573d15f5ee125c0c85652eafee7e
Kennedy Rose <kennedyianrose@gmail.com>
2020-01-23 14:56:26 -0600

    Fixed youtube link

--- packages/@smarterlabs.com/sanity/schemas/youtube-embed.js
+++ packages/@smarterlabs.com/sanity/schemas/youtube-embed.js
@@ -1,6 +1,19 @@
 export default {
 	name: `youtubeEmbed`,
 	title: `YouTube`,
-	type: `string`,
-	description: `The random string of letters after "v=" in the URL. Example: "https://www.youtube.com/watch?v=VwL-EFrjnGM" would be "EFrjnGM"`,
+	type: `object`,
+	fields: [
+		{
+			name: `id`,
+			title: `ID`,
+			type: `string`,
+			description: `The random string of letters after "v=" in the URL. Example: "https://www.youtube.com/watch?v=VwL-EFrjnGM" would be "EFrjnGM"`,
+		},
+		// {
+		// 	name: `order`,
+		// 	title: `Order`,
+		// 	type: `number`,
+		// 	description: `Used to determine the order that the links render in`,
+		// },
+	],
 }
